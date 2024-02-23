ca63fdb80a3f4bed226e44d883e02e8eaed4de74
Caique Souza Fonseca <c.souzaf@hotmail.com>
2024-02-22 21:27:23 -0300

    First commit

---
+++ focuson/src/main/java/com/api/focuson/FocusonApplication.java
@@ -0,0 +1,13 @@
+package com.api.focuson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusonApplication.class, args);
+	}
+
+}

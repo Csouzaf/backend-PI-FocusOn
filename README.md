972fc54a7f8bcd96b9e3f1370142358b6b8a6c3c
Caique Souza Fonseca <c.souzaf@hotmail.com>
2024-03-09 10:24:12 -0300

    Adição readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # backend-PI-FocusOn
+
+**Aplicação backend destinado ao Projeto Integrador 4ºSemestre**
+
+>1.O Padrão de Projeto é na arquitetura MVC; 
+
+>2.O projeto contem login, registro e rota home do usuário
---
+++ focuson/src/main/java/com/api/focuson/Repository/IRegistroRepository.java
@@ -0,0 +1,9 @@
+package com.api.focuson.view;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.api.focuson.Model.Registro;
+
+public interface IRegistroRepository extends JpaRepository<Registro, Long> {
+
+
+}
